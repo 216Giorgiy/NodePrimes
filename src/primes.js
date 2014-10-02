885836639cef8d57cce2137ed7c6a37d8c6ebebc
Steve Smith <steve@kentsmiths.com>
2014-10-01 22:46:02 -0400

    Implemented first pass on Sieve, which for now just finds 2 and marks all multiples of 2 as notprime.

--- src/primes.js
+++ src/primes.js
@@ -1,18 +1,57 @@
+CandidateStates = Object.freeze({UNKNOWN:0, NOTPRIME: 1, PRIME: 2});
+
 function Candidate(n) {
     this.value = n;
-    this.isPrime = false;
+    this.state = CandidateStates.UNKNOWN;
 }
-function Primes(maxSize) {
+function Primes(maxValue) {
     this.candidates = [];
-    this.maxSize = maxSize;
-    for(var i=0;i<this.maxSize;i++){
-        this.candidates[i]= new Candidate(i+1);
+    this.maxValue = maxValue;
+    for(var i=0;i<=this.maxValue;i++){
+        if(i<2){
+            this.candidates[i] = null;
+        }
+        else {
+            this.candidates[i] = new Candidate(i);
+        }
     }
 }
 
 Primes.prototype.sieve = function() {
-
+    function getFirstElementWithState(list, state) {
+        var index=-1;
+        for(var i=2;i<list.length;i++)
+        {
+            if(list[i].state === state) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+    // make first unknown value PRIME
+    var firstUnknownIndex = getFirstElementWithState(this.candidates, CandidateStates.UNKNOWN);
+    var foundPrime = 0;
+    if(firstUnknownIndex > 0) {
+        this.candidates[firstUnknownIndex].state = CandidateStates.PRIME;
+        foundPrime = this.candidates[firstUnknownIndex].value;
+    }
+    // iterate over candidates incrementing by foundPrime and mark NotPrime
+    for(var i=firstUnknownIndex+foundPrime;i<this.candidates.length;i+=foundPrime) {
+        this.candidates[i].state = CandidateStates.NOTPRIME;
+    }
 }
+
+Primes.prototype.countByState = function(state) {
+    var count = 0;
+    for(var i=2;i<this.candidates.length;i++) {
+        if(this.candidates[i].state === state) {
+            count++;
+        }
+    }
+    return count;
+}
+
 
 module.exports.Candidate = Candidate;
 module.exports.Primes = Primes;
--- src/test/primeTests.js
+++ src/test/primeTests.js
@@ -2,15 +2,27 @@ var assert = require("assert");
 var primes = require("../primes.js");
 
 describe('Primes', function() {
-    describe('when initially created with size 100', function() {
-        var myPrimes = new primes.Primes(100);
-        it('should have max size 100', function() {
-            assert.equal(100, myPrimes.maxSize);
+    describe('when initially created with maxValue 100', function() {
+        var maxValue = 100;
+        var myPrimes = new primes.Primes(maxValue);
+        it('should have max value 100', function() {
+            assert.equal(maxValue, myPrimes.maxValue);
         });
-        it('should have candidate list of size 100', function() {
-            assert.equal(100, myPrimes.candidates.length);
+        it('should have candidate list of size maxValue + 1', function() {
+            assert.equal(maxValue+1, myPrimes.candidates.length);
         });
+        it('should have 99 unknown candidates', function() {
+            assert.equal(maxValue-1, myPrimes.countByState(CandidateStates.UNKNOWN))
+        })
     });
+    describe('when sieve is called on primes with maxValue 100', function() {
+        var maxValue = 100;
+        var myPrimes = new primes.Primes(maxValue);
+        myPrimes.sieve();
+        it('should find one prime', function() {
+            assert.equal(1, myPrimes.countByState(CandidateStates.PRIME))
+        })
+    })
 });
 
 describe('Candidate', function() {
@@ -20,8 +32,8 @@ describe('Candidate', function() {
         it('should have a value of n', function() {
             assert.equal(n, candidate.value);
         });
-        it('should not be prime', function() {
-            assert.equal(false, candidate.isPrime);
+        it('should be in unknown state', function() {
+            assert.equal(CandidateStates.UNKNOWN, candidate.state);
         });
     });
 });
